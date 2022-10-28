449d61bcc2ec52f4f6a782d9883517eff1a864b7
vbalu01 <vhbal2001@gmail.com>
2022-10-28 12:41:11 +0200

    Spring api init - VHB

---
+++ Source/Spring/evp/src/main/java/com/webshop/evp/EvpApplication.java
@@ -0,0 +1,13 @@
+package com.webshop.evp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvpApplication.class, args);
+	}
+
+}
