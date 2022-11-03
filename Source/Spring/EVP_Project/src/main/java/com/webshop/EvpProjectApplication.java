e7860fa8597533fe2b57efc4464167097c37798e
vbalu01 <vhbal2001@gmail.com>
2022-11-03 21:44:55 +0100

    Public DB conn, app build cmd
    jdk 17+

--- Source/Spring/EVP_Project/src/main/java/com/webshop/EvpProjectApplication.java
+++ Source/Spring/EVP_Project/src/main/java/com/webshop/EvpProjectApplication.java
@@ -1,12 +1,22 @@
 package com.webshop;
 
+import org.apache.catalina.core.ApplicationContext;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EvpProjectApplication{
 	public static void main(String[] args) {
 		SpringApplication.run(EvpProjectApplication.class, args);
 	}
+	
+	/*@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+			System.out.println("App running");
+		};
+	}*/
 
 }
