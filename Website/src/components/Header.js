e4fcacd758f0b914d21c740eb7308d1ad014af80
KiKiHUN <kikimano001@gmail.com>
2022-11-03 20:17:33 +0100

    2.

--- Website/src/components/Header.js
+++ Website/src/components/Header.js
@@ -6,7 +6,7 @@ export default function Header() {
               <div class="row align-items-center">
                 
                 <div class="col-11 col-xl-2">
-                  <h1 class="mb-0 site-logo"><a href="index.html" class="text-white mb-0">IK webshop</a></h1>
+                  <h1 class="mb-0 site-logo"><a href="/" class="text-white mb-0">IK webshop</a></h1>
                 </div>
                 <div class="col-12 col-md-10 d-none d-xl-block">
                   <nav class="site-navigation position-relative text-right" role="navigation">
