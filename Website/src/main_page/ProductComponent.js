944126325b1580c3235adf1afae7bfcceb1dabc7
Mari <73820330+HMarii@users.noreply.github.com>
2022-11-13 14:15:29 +0100

    Product request proba 2 (úgy néz ki jó)

--- Website/src/main_page/ProductComponent.js
+++ Website/src/main_page/ProductComponent.js
@@ -1,6 +1,4 @@
 import React from 'react'
-import axios from 'axios'
-import ProductService from '../services/ProductService'
 
 class ProductComponent extends React.Component {
 
@@ -11,15 +9,10 @@ class ProductComponent extends React.Component {
 
 
    async componentDidMount() {
-    const response = await axios.get('http://localhost:8080/productsListing', {
-        headers : { 
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-           }
-    })
+    const response = await fetch('/productsListing')
     const body = await response.json();
     this.setState({products: body});
-    console.log(body);
+    //console.log(body);
         
     }
     
@@ -53,11 +46,12 @@ class ProductComponent extends React.Component {
                         {
                             products.map(
                                 product =>
-                                <tr key={product.Id}>
-                                    <td>{product.name}</td>
-                                    <td>{product.description}</td>
-                                    <td>{product.price}</td>
-                                    <td>{product.sale_price}</td>
+                                <tr key={product.p.id}>
+                                    <td>{product.p.id}</td>
+                                    <td>{product.p.name}</td>
+                                    <td>{product.p.description}</td>
+                                    <td>{product.p.price}</td>
+                                    <td>{product.p.sale_price}</td>
 
                                 </tr>
                             )
